0a32b5775c46c8d7a626a1cccd9c357ae6974d1d
Martin <Martin@Macintosh.local>
2011-12-19 10:45:43 +0900

    Initial Commit

---
+++ Simple Sim/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Simple Sim
+//
+//  Created by O'Connor Martin on 19/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Simple Sim/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  Simple Sim
+//
+//  Created by O'Connor Martin on 19/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
